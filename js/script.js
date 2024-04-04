c4f28789cce685bdd5f781e9c9cb1cee796f2945
sahil110059k@gmail.com <sa98547@gmail.com>
2024-04-04 19:26:49 +0530

    update navigation bar

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+
+
+//Make mobile navigation work
+const btnNavElement = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
+
+btnNavElement.addEventListener("click", function () {
+  headerElement.classList.toggle("nav-open");
+})
